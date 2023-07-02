3a6bd5c70fc9b0161ca3a5bf22fce34a8c0f98ae
nneddk <meljandre.mc@gmail.com>
2023-07-02 23:51:18 +0800

    started work on pieces

--- scripts/index.js
+++ scripts/index.js
@@ -1,46 +1,82 @@
-console.log('Hello World!');
 const gameBoard = document.getElementById('game-board');
 const chessBoard =(()=>{
 
-    let chessBoardArray = [];
-
-    const emptyBoard = () =>{
-        //fills up the board with 'empty positions'
-        for (let y = 1; y <= 8; y++){
-            let chessBoardTileData = [];
-            for(let x = 1; x <= 8; x++){
-                chessBoardTileData.push('_');
-            }
-            chessBoardArray.push(chessBoardTileData);
-        }
-        console.log(chessBoardArray);
-    }
-
+    let chessBoardData = [];
+    let tempHolder = [];
+    let isMoving = false;
+    let movingData = {
+        piece: null,
+        x: null,
+        y: null
+    };
     //creates board element
     const makeBoard = () =>{
         while (gameBoard.hasChildNodes()) {
             gameBoard.removeChild(gameBoard.lastChild);
           }
         
-        for(let y = 8; y >= 1; y--){
-            for(let x = 1; x <= 8; x++){
+        for(let y = 0; y < 8; y++){
+            let chessBoardTileContainer = document.createElement('div');
+            chessBoardTileContainer.classList.add('tile-container');
+            let chessBoardTileData = [];
+            for(let x = 0; x < 8; x++){
+                
                 let chessBoardTileDiv = document.createElement('div');
                 chessBoardTileDiv.classList.add('tile');
-                chessBoardTileDiv.style.backgroundColor = ((y+x)%2?'rgb(238,238,210':'rgb(118,150,86)');
+                chessBoardTileDiv.style.backgroundColor = (((y+1) + (x+1))%2?'rgb(118,150,86)':'rgb(238,238,210');
                 //object for easier data manipulation
                 let TileData ={
-                    yCoord: y,
-                    xCoord: String.fromCharCode(96 + x),
-                    isEmpty: (!chessBoardTileDiv.hasChildNodes())
+                    isEmpty: true,
+                    tileLocation: chessBoardTileDiv,
+ 
                 };
-                chessBoardTileDiv.onclick = function(){
-                    console.log(TileData.isEmpty);
+                chessBoardTileDiv.onclick = ()=>{
+                    if(movingData.piece){ 
+                        movePiece(x,y);
+                    }
                 }
-                gameBoard.append(chessBoardTileDiv);
+                chessBoardTileContainer.append(chessBoardTileDiv);
+                chessBoardTileData.push(TileData);
             }
-            
+            gameBoard.append(chessBoardTileContainer);
+            chessBoardData.push(chessBoardTileData);
         }
     }
-    return {makeBoard};
+    const movePiece = (x,y) =>{
+        if(movingData.x != x || movingData.y != y){
+            movingData.x = x;
+            movingData.y = y;
+            chessBoardData[y][x].tileLocation.append(movingData.piece);
+            chessBoardData[tempHolder[1]][tempHolder[0]].isEmpty = true;
+            chessBoardData[y][x].isEmpty = false;
+            console.log(chessBoardData);
+        }
+            movingData.piece = null;
+            movingData.x = null;
+            movingData.y = null;
+    }
+    const pieceMaker = (x,y,id) =>{
+        let gameBoardArrayData = gameBoard.querySelectorAll('.tile-container');
+        let piece = document.createElement('div');
+        piece.classList.add('piece');
+        piece.textContent = 'piece';
+        let pieceData = {
+            id:id,
+        }
+        piece.onclick=(e) =>{
+            console.log(pieceData.id);
+            tempHolder = [x,y];
+            //IMPORTANT TO STOP PARENT FROM TRIGGERING NULL 
+            e.stopPropagation();
+            movingData.piece = piece;
+        }
+        chessBoardData[y][x].isEmpty = false;
+        gameBoardArrayData[y].childNodes[x].append(piece);
+    }
+
+    
+    return {makeBoard,pieceMaker};
 })();
 chessBoard.makeBoard();
+chessBoard.pieceMaker(0,6, 'rook');
+chessBoard.pieceMaker(1,6, 'pawn');
