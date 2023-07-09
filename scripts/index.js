9663f087e446f1ed7e51b4bf3e076eda0ea42379
nneddk <meljandre.mc@gmail.com>
2023-07-09 11:30:01 +0800

    added functionality to footer buttons

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,12 @@
 import { chessBoard } from "./chessboard.js";
 chessBoard.makeBoard();
 chessBoard.generateGame();
+const resetBtn = document.getElementById('reset-btn');
+const indicator = document.getElementById("indicator");
+resetBtn.onclick=()=>{
+    if(confirm('Reset the board?')){
+        indicator.textContent = '';
+        chessBoard.makeBoard();
+        chessBoard.generateGame();
+    }
+}
